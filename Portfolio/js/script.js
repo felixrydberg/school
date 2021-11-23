998523b0de3768ce9e26587b4a80dc49c66c1b0a
Felix Rydberg <rydis789@gmail.com>
2021-11-23 11:44:15 +0100

    piss and shit

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -49,22 +49,30 @@ imgname = [
     "logo.png",
     "logo.png",
     "logo.png"];
-const portfolioParent = document.querySelector(".portfolio");
-if (links.length == names.length && links.length == desc.length && links.length == imgname.length) {
-    for(let i = 0; i < links.length; i++) {
-        const article = document.createElement("article"),
-        img = document.createElement("img"),
-        h1 = document.createElement("h1"),
-        p = document.createElement("p");
-        portfolioParent.appendChild(article);
-        article.appendChild(img)
-        article.appendChild(h1);
-        article.appendChild(p);
-        img.setAttribute('src', 'image/portfolio/' + imgname[i]);
-        h1.innerHTML = names[i];
-        p.innerHTML = desc[i];
+
+spawnPortfolio(links, names, desc, imgname);
+
+function spawnPortfolio(links, names, desc, imgname) {
+    const portPar = document.querySelector(".portfolio");
+    if (links.length == names.length && links.length == desc.length && links.length == imgname.length) {
+        for(let i = 0; i < links.length; i++) {
+            const article = document.createElement("article"),
+            img = document.createElement("img"),
+            h1 = document.createElement("h1"),
+            p = document.createElement("p");
+            let color = (20 / links.length * i) + 30; 
+            portPar.appendChild(article);
+            article.appendChild(img)
+            article.appendChild(h1);
+            article.appendChild(p);
+            img.setAttribute('src', 'image/portfolio/' + imgname[i]);
+            h1.innerHTML = names[i];
+            p.innerHTML = desc[i];
+            console.log(color);
+            article.style.background = `hsl 0, 0%, ${color}%)`
+        }
     }
-}
-else {
-    console.log("Array lenght failure");
+    else {
+        console.log("Array lenght failure");
+    }
 }
